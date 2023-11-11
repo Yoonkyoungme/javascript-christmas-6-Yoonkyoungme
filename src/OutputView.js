8a9f8ff1bd1630e80a4a189099a9dea8c44eb7fe
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2023-11-11 23:01:33 +0900

    feat: 크리스마스 프로모션 소개 문구 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,16 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
+import { Console } from "@woowacourse/mission-utils";
+import { PRINT } from "./utils/messages.js";
+
+const OutputView = {
+  printIntro() {
+    Console.print(PRINT.INTRO);
+  },
+
+  printMenu() {
+    Console.print("<주문 메뉴>");
     // ...
-}
+  },
+  // ...
+};
+
+export default OutputView;
---
+++ src/utils/messages.js
@@ -0,0 +1,9 @@
+const READ = {};
+
+const PRINT = {
+  INTRO: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
+};
+
+const ERROR = {};
+
+export { READ, PRINT, ERROR };
