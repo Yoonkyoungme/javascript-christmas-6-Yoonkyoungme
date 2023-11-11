b10fe53564773139e5aed3adb2205c5d1c6af5a0
Yoonkyoungme <dev.yoonkyoung@gmail.com>
2023-11-11 23:12:55 +0900

    feat: 방문 날짜 입력 받기

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import OutputView from "./OutputView.js";
 class App {
   async run() {
     OutputView.printIntro();
+    await this.getInputData();
+  }
+
+  async getInputData() {
+    const date = await InputView.readDate();
   }
 }
 
